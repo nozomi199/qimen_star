b734e636e1e9909f5123be714dbcd15f9770deca
nozomi199 <nozomi199@gmail.com>
2015-01-21 08:36:31 +0800

    slove bazi's minor fortune(八字小運) calculation

--- _js/bazi.js
+++ _js/bazi.js
@@ -52,8 +52,8 @@ if(typeof(QIMEN_STAR) == "undefined") var QIMEN_STAR = {};
       } else {// 陽女, 陰男
         for(var i = 1; i <= start_small; i++) {
           _out.small.push(
-            "甲乙丙丁戊己庚辛壬癸".charAt((tinIdx-i)%10) +
-            "子丑寅卯辰巳午未申酉戌亥".charAt((deiIdx-i)%12)
+            "甲乙丙丁戊己庚辛壬癸".charAt((tinIdx-i+10)%10) +
+            "子丑寅卯辰巳午未申酉戌亥".charAt((deiIdx-i+12)%12)
           );
         }
       }
